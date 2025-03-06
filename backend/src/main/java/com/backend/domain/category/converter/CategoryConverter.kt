d0c2ecd49229af4baa41f81803b6cb72dc07a5d0
Gonhub9 <ho_gok@naver.com>
2025-03-06 11:05:43 +0900

    refactor: CategoryConverter 코틀린으로 변환

--- backend/src/main/java/com/backend/domain/category/converter/CategoryConverter.kt
+++ backend/src/main/java/com/backend/domain/category/converter/CategoryConverter.kt
@@ -1,39 +1,34 @@
-package com.backend.domain.category.converter;
+package com.backend.domain.category.converter
 
-import com.backend.domain.category.dto.request.CategoryRequest;
-import com.backend.domain.category.dto.response.CategoryResponse;
-import com.backend.domain.category.entity.Category;
-import java.util.List;
-import java.util.stream.Collectors;
-import org.springframework.stereotype.Component;
+import com.backend.domain.category.dto.request.CategoryRequest
+import com.backend.domain.category.dto.response.CategoryResponse
+import com.backend.domain.category.entity.Category
+import org.springframework.stereotype.Component
 
 @Component
-public class CategoryConverter {
+object CategoryConverter {
 
     // 카테고리 리스트 매핑
-    public static List<CategoryResponse> toResponseList(List<Category> categoryList) {
-        return categoryList.stream()
-                .map(category -> new CategoryResponse(
-                        category.getId(),
-                        category.getName(),
-                        category.getCreatedAt(),
-                        category.getModifiedAt()
-                ))
-                .collect(Collectors.toList());
-    }
+    fun toResponseList(categoryList : List<Category>): List<CategoryResponse> =
+        categoryList.map { category ->
+            CategoryResponse(
+                id = category.id,
+                name = category.name,
+                createdAt = category.createdAt,
+                modifiedAt = category.modifiedAt
+            )
+        }
 
     // 카테고리 매핑 (단일 객체)
-    public static CategoryResponse toResponse(Category category) {
-        return new CategoryResponse(
-                category.getId(),
-                category.getName(),
-                category.getCreatedAt(),
-                category.getModifiedAt()
-        );
-    }
+    fun toResponse(category: Category): CategoryResponse =
+        CategoryResponse(
+            id = category.id,
+            name = category.name,
+            createdAt = category.createdAt,
+            modifiedAt = category.modifiedAt
+        )
 
     // 카테고리 엔티티로 바꾸는 메서드
-    public static Category toEntity(CategoryRequest categoryRequest) {
-        return new Category(categoryRequest.getName());
-    }
+    fun toEntity(categoryRequest: CategoryRequest): Category =
+        Category(name = categoryRequest.name)
 }
--- backend/src/main/java/com/backend/domain/category/dto/response/CategoryResponse.kt
+++ backend/src/main/java/com/backend/domain/category/dto/response/CategoryResponse.kt
@@ -3,7 +3,7 @@ package com.backend.domain.category.dto.response
 import java.time.ZonedDateTime
 
 data class CategoryResponse(
-    val id: Long = 0L,
+    val id: Long ?= 0L,
     val name: String = "",
     val createdAt: ZonedDateTime = ZonedDateTime.now(),
     val modifiedAt: ZonedDateTime = ZonedDateTime.now()
