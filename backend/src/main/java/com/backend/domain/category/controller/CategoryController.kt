4c4c816b0fa866447960846f3cebf3e3ab4635ea
Gonhub9 <ho_gok@naver.com>
2025-03-06 14:51:13 +0900

    refactor: CategoryController 코틀린으로 변환

--- backend/src/main/java/com/backend/domain/category/controller/CategoryController.kt
+++ backend/src/main/java/com/backend/domain/category/controller/CategoryController.kt
@@ -1,55 +1,50 @@
-package com.backend.domain.category.controller;
+package com.backend.domain.category.controller
 
-import com.backend.domain.category.dto.request.CategoryRequest;
-import com.backend.domain.category.dto.response.CategoryResponse;
-import com.backend.domain.category.service.CategoryService;
-import com.backend.global.response.GenericResponse;
-import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.backend.domain.category.dto.request.CategoryRequest
+import com.backend.domain.category.dto.response.CategoryResponse
+import com.backend.domain.category.service.CategoryService
+import com.backend.global.response.GenericResponse
+import lombok.RequiredArgsConstructor
+import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/category")
 @RequiredArgsConstructor
-public class CategoryController {
-
-    private final CategoryService categoryService;
+class CategoryController(
+    private val categoryService: CategoryService
+) {
 
     // 카테고리 전체 조회
     @GetMapping
-    public GenericResponse<List<CategoryResponse>> getAllCategory() {
-        List<CategoryResponse> categoryList = categoryService.categoryList();
-        return GenericResponse.ok(categoryList);
+    fun getAllCategory(): GenericResponse<List<CategoryResponse>> {
+        val categoryList = categoryService.categoryList()
+        return GenericResponse.ok(categoryList)
     }
 
     // 카테고리 추가 (관리자만 가능)
     @PostMapping
-    public GenericResponse<CategoryResponse> createCategory(@RequestBody @Validated CategoryRequest categoryRequest) {
-        CategoryResponse categoryResponse = categoryService.createCategory(categoryRequest);
-        return GenericResponse.ok(HttpStatus.CREATED.value(), categoryResponse);
+    fun createCategory(@RequestBody @Validated categoryRequest: CategoryRequest):
+            GenericResponse<CategoryResponse>  {
+        val categoryResponse = categoryService.createCategory(categoryRequest)
+        return GenericResponse.ok(HttpStatus.CREATED.value(), categoryResponse)
     }
 
     // 카테고리 수정 (관리자만 가능)
     @PatchMapping("/{id}")
-    public GenericResponse<CategoryResponse> updateCategory(
-            @RequestBody @Validated CategoryRequest categoryRequest, @PathVariable("id") Long id) {
-        CategoryResponse categoryResponse = categoryService.updateCategory(id, categoryRequest);
-        return GenericResponse.ok(categoryResponse);
+    fun updateCategory(
+        @PathVariable("id") id: Long,
+        @RequestBody @Validated  categoryRequest: CategoryRequest
+            ): GenericResponse<CategoryResponse> {
+        val categoryResponse = categoryService.updateCategory(id, categoryRequest)
+        return GenericResponse.ok(categoryResponse)
     }
 
     // 카테고리 삭제 (관리자만 가능)
     @DeleteMapping("/{id}")
-    public GenericResponse<Void> deleteCategory(@PathVariable("id") Long id) {
-        categoryService.deleteCategory(id);
-        return GenericResponse.ok();
+    fun deleteCategory(@PathVariable("id") id: Long):  GenericResponse<Void>  {
+        categoryService.deleteCategory(id)
+        return GenericResponse.ok()
     }
 }
--- backend/src/main/java/com/backend/domain/category/repository/CategoryRepository.kt
+++ backend/src/main/java/com/backend/domain/category/repository/CategoryRepository.kt
@@ -2,7 +2,7 @@ package com.backend.domain.category.repository
 
 import com.backend.domain.category.entity.Category
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
+import java.util.Optional
 
 interface CategoryRepository : JpaRepository<Category, Long> {
 
